8638b08281b3ac4fae9f46e1fd1ef177a3ef5dbd
erikaosgue <1476@holbertonschool.com>
2020-12-03 01:06:35 -0500

    Program that Prints the lenght of the array args passing arguments from command line

---
+++ 0x00-java-hello_world/4-stringLength.java
@@ -0,0 +1,12 @@
+
+
+/**
+ * The StringLength class implements an application that simply prints 
+ * the length of the array args to the standard output
+ */
+
+class StringLength {
+	public static void main(String[] args){
+		System.out.println(args.length);
+	}
+}
