8d607d147e07ac1af25dfb117c3df467697e2e95
erikaosgue <1476@holbertonschool.com>
2020-12-03 01:04:45 -0500

    Program that prints a number with a variable

---
+++ 0x00-java-hello_world/1-printNumber.java
@@ -0,0 +1,13 @@
+
+
+/**
+ * The PrintNumber class implements an application that
+ * simply prints "2020 Was the covid year!" to standard output.
+ */
+
+class PrintNumber {
+	public static void main( String []args ) {
+		int number = 2020;
+		System.out.println(number + " Was the covid year!");
+	}
+}
