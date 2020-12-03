9bf53d921c104d821a716a65288e5b4ee91bcfe9
erikaosgue <1476@holbertonschool.com>
2020-12-03 01:05:35 -0500

    Program that prints a String

---
+++ 0x00-java-hello_world/3-printString.java
@@ -0,0 +1,14 @@
+
+
+/**
+ * The PrintString class implements an application that simply prints 
+ * "Today I am learning java" to the standar output
+ */
+
+class PrintString {
+	public static void main(String []args){
+	String a = "Today I am learning";
+	String b = " Java";
+	System.out.println(a + b);
+	}
+}
