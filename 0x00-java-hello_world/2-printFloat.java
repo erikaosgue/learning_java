014afd7c10ed97e90a3b881cc811778ef0e63303
erikaosgue <1476@holbertonschool.com>
2020-12-03 01:05:11 -0500

    Program that prints a float number

---
+++ 0x00-java-hello_world/2-printFloat.java
@@ -0,0 +1,13 @@
+
+/**
+ * The PrintFloat class implements an application that
+ * simply prints "3.1416 represents the pi number" to standard output.
+ */
+
+class PrintFloat {
+	public static void main(String []args){
+		float number = 3.1416f;
+		System.out.println(number + " represents the pi number");
+	}
+}
+
